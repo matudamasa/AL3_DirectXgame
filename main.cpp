430bd09dd367384229c41a12b8037068c171d4ba
MasatuguMatuda <k021g1292@g.neec.ac.jp>
2022-04-13 11:01:36 +0900

    01_01 名前を変更

--- main.cpp
+++ main.cpp
@@ -19,7 +19,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	// ゲームウィンドウの作成
 	win = WinApp::GetInstance();
 	win->CreateGameWindow("LE2D_13_マツダ_マサツグ_AL3");
-
+	
 	// DirectX初期化処理
 	dxCommon = DirectXCommon::GetInstance();
 	dxCommon->Initialize(win);
